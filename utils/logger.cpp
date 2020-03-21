d4449d6034266caad5a746722e5ef5a2ffdc6b26
Ziyan Jerry Chen <jerryc443@gmail.com>
2020-03-21 18:56:02 -0400

    change delimiter size

--- utils/logger.cpp
+++ utils/logger.cpp
@@ -4,12 +4,13 @@
 using std::setw;
 template<typename T> using BasicOutStream = std::basic_ostream<T>;
 
-constexpr auto DELIMITER_COUNT = 16;
+constexpr auto DELIMITER_COUNT = 64;
 constexpr auto DELIMITER_CHAR  = '-';
 
 template<typename T>
 inline auto &prefix(BasicOutStream<T> &stream) {
-  return stream << setw(DELIMITER_COUNT) << cout.fill(DELIMITER_CHAR) << '\n';
+  cout.fill(DELIMITER_CHAR);
+  return stream << setw(DELIMITER_COUNT) << '\n';
 }
 
 [[maybe_unused]] ostream &log_e() {
